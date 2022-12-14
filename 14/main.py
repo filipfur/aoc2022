7444d19326bf5f33eac4cde9dec1070dfad62d0b
filipfur <filipfur@live.se>
2022-12-14 20:30:49 +0100

    day 14

--- 14/main.py
+++ 14/main.py
@@ -5,9 +5,23 @@ class Particle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.active = True
 
-    def update(self):
-        self.y += 1
+    def update(self, grid):
+        if not self.active:
+            return
+        if grid.getTile(self.x, self.y + 1) == None:
+            self.y += 1
+        elif grid.getTile(self.x - 1, self.y + 1) == None:
+            self.x -= 1
+            self.y += 1
+        elif grid.getTile(self.x + 1, self.y + 1) == None:
+            self.x += 1
+            self.y += 1
+        else:
+            grid.setTile(self.x, self.y, 'o')
+            self.active = False
+
 
 class Grid:
 
@@ -16,11 +30,12 @@ class Grid:
         self.max=[-100,-100]
         self.tiles = {}
 
-    def setTile(self, x, y, c):
-        self.min[0] = min(self.min[0], x)
-        self.min[1] = min(self.min[1], y)
-        self.max[0] = max(self.max[0], x)
-        self.max[1] = max(self.max[1], y)
+    def setTile(self, x, y, c, updateminmax=True):
+        if updateminmax:
+            self.min[0] = min(self.min[0], int(x))
+            self.min[1] = min(self.min[1], int(y))
+            self.max[0] = max(self.max[0], int(x))
+            self.max[1] = max(self.max[1], int(y))
         if not y in self.tiles:
             self.tiles[y] = {}
         self.tiles[y][x] = c
@@ -40,6 +55,7 @@ class Grid:
                 self.setTile(cur[0], cur[1], '#')
                 cur[0] += delta[0]
                 cur[1] += delta[1]
+            self.setTile(cur[0], cur[1], '#')
 
     def getTile(self, x, y):
         if not y in self.tiles:
@@ -51,8 +67,8 @@ class Grid:
 
     def doPrint(self):
         output = ""
-        for y in range(self.min[1], self.max[1] + 1):
-            for x in range(self.min[0], self.max[0] + 1):
+        for y in range(self.min[1] - 1, self.max[1] + 2):
+            for x in range(self.min[0] - 1, self.max[0] + 2):
                 tile = self.getTile(x, y)
                 output += ('.' if tile == None else tile)
             output += "\n"
@@ -74,7 +90,6 @@ grid.doPrint()
 particles = []
 
 
-exit(0)
 #pyplot.ion()
 
 
@@ -83,15 +98,46 @@ pyplot.gca().invert_yaxis()
 for seg in segments:
     x = [e[0] for e in seg]
     y = [e[1] for e in seg]
-    pyplot.plot(x, y, linewidth=10)
+    pyplot.plot(x, y, linewidth=4)
 
-for i in range(20):
-    particles.append(Particle(500, 0))
-    for p in particles:
-        p.update()
+finalLevel = False
+lastX = 0
+lastY = 0
+for i in range(1000000):
+    #particles.append(Particle(500, 0))
+    #for p in particles:
+    #    p.update(grid)
+    p = Particle(500, 0)
+    particles = [p]
+    while p.active:
+        p.update(grid)
+        if p.y > grid.max[1]:
+            lastX = p.x
+            lastY = p.y
+            break
+    if p.y > grid.max[1]:
+        if not finalLevel:
+            grid.placeSegment([[lastX - 1000, lastY + 1], [lastX + 1000, lastY + 1]])
+            print("You unlocked the final level!")
+            #grid.doPrint()
+            finalLevel = True
+        else:
+            p = Particle(500,0)
+            while p.active:
+                p.update(grid)
+                grid.setTile(p.x, p.y, "~", False)
+                if p.y > grid.max[1]:
+                    break
+            print("abyss encountered after {} iterations".format(i))
+            break
     px = [p.x for p in particles]
     py = [p.y for p in particles]
-    pyplot.scatter(px, py, marker='o', s=40)
-    pyplot.pause(0.5)
+    pyplot.scatter(px, py, marker='o', s=10)
+    if grid.getTile(500, 0) == 'o':
+        print("Finally at rest after {} iterations.".format(i))
+        break
+    #pyplot.pause(0.2)
 
-pyplot.show()
+#grid.doPrint()
+
+#pyplot.show()
